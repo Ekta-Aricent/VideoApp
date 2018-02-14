c576360a83ac7aea22b64375f7f2bd2ac0de751c
Ekta-Aricent <36473855+Ekta-Aricent@users.noreply.github.com>
2018-02-14 16:32:07 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = '172.19.74.136'
+PORT = 8089
+ADDR = (HOST,PORT)
+BUFSIZE = 4096
+videofile = "/root/ekta/video/Demo_video.mp4"
+
+bytes = open(videofile).read()
+
+print len(bytes)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+client.send(bytes)
+
+client.close()
